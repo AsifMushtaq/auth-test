3117d5fa48a1bfa1ad8b1e0518c9686110dcb6a3
Asif Mushtaq <asif.axi@gmail.com>
2016-02-17 09:50:45 +1100

    dirty finish

---
+++ auth-app/obj/Release/Package/PackageTmp/app/src/login/login.js
@@ -0,0 +1,57 @@
+﻿(function () {
+
+    angular
+         .module('users')
+         .controller('Login', [
+            'userService', '$state', 'store',
+            Login
+         ]);
+
+    /**
+     * Main Controller for the Angular Material Starter App
+     * @param $scope
+     * @param $mdSidenav
+     * @param avatarsService
+     * @constructor
+     */
+    function Login(userService, $state, store) {
+        var self = this;
+
+        self.username = store.get('username');;
+        self.login = doLogin;
+        // Load all registered users
+
+        //userService
+        //      .loadAllUsers()
+        //      .then(function (users) {
+        //          self.users = [].concat(users);
+        //          self.selected = users[0];
+        //      });
+
+        // *********************************
+        // Internal methods
+        // *********************************
+
+        /**
+         * Hide or Show the 'left' sideNav area
+         */
+        function doLogin() {
+            store.remove('username');
+            store.set('username', self.username); 
+            $state.go('user');
+           
+        }
+
+        /**
+         * Select the current avatars
+         * @param menuId
+         */
+        //function selectUser(user) {
+        //    self.selected = angular.isNumber(user) ? $scope.users[user] : user;
+        //}
+
+
+
+    }
+
+})();
---
+++ auth-app/obj/Release/Package/PackageTmp/app/src/user/user.js
@@ -0,0 +1,81 @@
+﻿(function () {
+
+    angular
+         .module('users')
+         .controller('User', [
+            'userService', 'store', '$firebaseObject', '$timeout', '$mdDialog',
+            User
+         ]);
+
+    /**
+     * Main Controller for the Angular Material Starter App
+     * @param $scope
+     * @param $mdSidenav
+     * @param avatarsService
+     * @constructor
+     */
+    function User(userService, store, $firebaseObject, $timeout, $mdDialog) {
+        var self = this;
+
+        self.username = store.get('username');
+        self.send = send;
+        self.varified = false;
+
+        var ref = new Firebase("https://domain-auth.firebaseio.com/verify-receive");
+        self.varificationCode = $firebaseObject(ref);
+
+        $timeout(function () {
+            self.showConfirm();
+        }, 3000);
+        
+
+        function send() {
+            var ref2 = new Firebase("https://domain-auth.firebaseio.com/verify-receive");
+            var sendMessage = $firebaseObject(ref2);
+            //self.varificationCode.$value = {
+            //    verification_key: "89"
+            //};;
+            sendMessage.$value = { email: guid() };
+            sendMessage.$save();
+            self.varified = true;
+        }
+
+
+        self.showConfirm = function (ev) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            var confirm = $mdDialog.confirm()
+                  .title('Press verify to authorise your request.')
+                  .textContent("You've just made a request to login using Passwordless Authentication on a DomainGroup website.")
+                  .ariaLabel('Lucky day')
+                  .targetEvent(ev)
+                  .ok('Verify!')
+                  .cancel('Cancel');
+            $mdDialog.show(confirm).then(function () {
+                self.status = 'Verified';
+            }, function () {
+                self.status = 'Canceled';
+            });
+        };
+
+
+        /**
+         * Select the current avatars
+         * @param menuId
+         */
+        //function selectUser(user) {
+        //    self.selected = angular.isNumber(user) ? $scope.users[user] : user;
+        //}
+
+        function guid() {
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                  .toString(16)
+                  .substring(1);
+            }
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+              s4() + '-' + s4() + s4() + s4();
+        }
+
+    }
+
+})();
